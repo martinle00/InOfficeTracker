1910a88e499b8c78ec9555d7e2d0320338491735
Martin Le <martinleunsw@gmail.com>
2025-07-01 17:49:23 +1000

    ignore ssm for test package

--- src/test/java/com/project/InOfficeTracker/InOfficeTrackerApplicationTests.java
+++ src/test/java/com/project/InOfficeTracker/InOfficeTrackerApplicationTests.java
@@ -2,12 +2,10 @@ package com.project.InOfficeTracker;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class InOfficeTrackerApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
 
 }
