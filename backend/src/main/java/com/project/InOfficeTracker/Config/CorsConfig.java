d7630a9b757549801509c54b497b8a4cd0753e59
Martin Le <martinleunsw@gmail.com>
2025-07-06 17:18:32 +1000

    adding cors changes

--- backend/src/main/java/com/project/InOfficeTracker/Config/CorsConfig.java
+++ backend/src/main/java/com/project/InOfficeTracker/Config/CorsConfig.java
@@ -1,5 +1,7 @@
 package com.project.InOfficeTracker.Config;
 
+import com.project.InOfficeTracker.Services.ParameterStoreService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.cors.CorsConfiguration;
@@ -8,10 +10,18 @@ import org.springframework.web.filter.CorsFilter;
 
 @Configuration
 public class CorsConfig {
+
+    @Autowired
+    ParameterStoreService parameterStoreService;
+
+    String localhostUrl = parameterStoreService.getSecret("LocalHostURL");
+    String prodUrl = parameterStoreService.getSecret("ProdURL");
+
     @Bean
     public CorsFilter corsFilter() {
         CorsConfiguration config = new CorsConfiguration();
-        config.addAllowedOrigin("http://localhost:3000");
+        config.addAllowedOrigin(localhostUrl);
+        config.addAllowedOrigin(prodUrl);
         config.addAllowedHeader("*");
         config.addAllowedMethod("*");
         config.setAllowCredentials(true);
